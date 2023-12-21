8759d8f6958c74e02d0a29c2b2685034381724f4
ChuaTeBongToi <hbaoanh2k9@gmail.com>
2023-12-21 19:48:42 +0700

    ff

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -41,7 +41,7 @@ const Nav = () => {
               Sign Out
             </button>
 
-            <Link href="/profile">
+            <Link href={`profile/${session?.user.id}`}>
               <Image src={session?.user.image} width={37} height={37} className='rounded-full' alt="profile" />
             </Link>
           </div>) :
